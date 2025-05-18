698d23e3587941e9fffc7e45ce249216f99c0149
Muhammad Umair <mumair9021@gmail.com>
2025-05-18 18:28:29 +0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav className="w-full flex items-center justify-between px-8 py-4 bg-primary text-primary-foreground shadow-md">
-      <div className="flex text-2xl font-bold pl-15">Bookbuddy</div>
+      
+      {/* Center: Title */}
+      <h1 className="flex text-2xl font-bold pl-15">Bookbuddy</h1>
+      
+      {/* Left side: Search Bar */}
+      <div className="flex items-center mr-150">
+        <input
+          type="text"
+          placeholder="Search books..."
+          className="w-104 px-4 py-2 rounded-md text-white bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      {/* Right side: Links */}
       <div className='flex items-center space-x-6'>
         <a href="#" className="hover:underline">Sell</a>
         <a href="#" className="hover:underline">Buy</a>
